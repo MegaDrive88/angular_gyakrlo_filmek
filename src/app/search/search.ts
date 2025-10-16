d111283b2cfd5de6985fdbe5027690e91db40c23
Podlipnik Ádám <adam.podlipnik@raabdigital.hu>
2025-10-16 09:53:33 +0200

    genre filter done

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { App } from '../app';
 import { Genre } from '../interfaces/movieDetails.interface';
 import Movie from '../interfaces/movie.interface';
 import { TranslatePipe } from '@ngx-translate/core';
+import { MatSelectModule, MatSelect } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'search-root',
-  imports: [CommonModule, FormsModule, RouterOutlet, TranslatePipe],
+  imports: [CommonModule, FormsModule, RouterOutlet, TranslatePipe, MatFormFieldModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './search.html',
   styleUrl: '../app.scss'
 })
-export class Search extends App implements OnInit {
+export class Search extends App implements OnInit, AfterViewInit {
     protected userEmail = localStorage.getItem("userEmail")
     protected movieTitle = localStorage.getItem("movieTitle") ?? ""
     protected logout(){
@@ -21,7 +23,10 @@ export class Search extends App implements OnInit {
         this.router.navigateByUrl("")
     }
     protected movies:Movie[] = []
-    private genres:Genre[] = []
+    protected genres:Genre[] = []
+    @ViewChild('genreSelector') genreSelector!:MatSelect;
+    @ViewChild('languageSelector') languageSelector!:MatSelect;
+    
     protected onType(){
         localStorage.setItem("movieTitle", this.movieTitle)
         if (this.movieTitle.length < 3) return
@@ -30,6 +35,13 @@ export class Search extends App implements OnInit {
             .subscribe({
                 next: (data) => {
                     this.movies = (data.results as Movie[])
+                    if(this.genreSelector.value.length > 0) 
+                        this.movies = this.movies.filter(x => 
+                            x.genre_ids.some(y=> this.genreSelector.value.includes(y))
+                        )
+
+                    if(this.languageSelector.value.length > 0)
+                        this.movies = this.movies.filter(x => this.languageSelector.value.includes(x.original_language))
                     this.cdr.detectChanges()
                 },
                 error: (err) => {
@@ -55,6 +67,10 @@ export class Search extends App implements OnInit {
             this.onType()
             this.cdr.detectChanges()
         })
+    }
+    ngAfterViewInit(): void {
+        this.genreSelector.value = []
+        this.languageSelector.value = []
         this.onType()
     }
     protected movieClicked(movieId :number){
